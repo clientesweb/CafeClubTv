3881c34aad33131070cf8e35b2b60ba9a492af62
clientesweb <alexisifran32@gmail.com>
2024-09-29 15:53:07 -0300

    Actualizar Header.js

--- js/components/Header.js
+++ js/components/Header.js
@@ -1,11 +1,6 @@
-// components/Header.js
-class Header {
-    constructor() {
-        this.element = document.createElement('header');
-    }
-
-    render() {
-        this.element.innerHTML = `
+export default function Header() {
+    return `
+        <header>
             <div class="logo">
                 <img src="images/logi.svg" alt="Logo de Cafe Club Tv">
             </div>
@@ -14,30 +9,41 @@ class Header {
                     <i class="fas fa-download"></i> Instalar CafeClubTV App
                 </button>
             </div>
-        `;
-        return this.element;
-    }
-
-    setupInstallButton() {
-        let deferredPrompt;
-        const installButton = this.element.querySelector('#install-button');
-
-        window.addEventListener('beforeinstallprompt', (e) => {
-            e.preventDefault();
-            deferredPrompt = e;
-            installButton.style.display = 'block';
-        });
-
-        installButton.addEventListener('click', async () => {
-            if (deferredPrompt) {
-                deferredPrompt.prompt();
-                const { outcome } = await deferredPrompt.userChoice;
-                if (outcome === 'accepted') {
-                    console.log('User accepted the install prompt');
-                }
-                deferredPrompt = null;
-            }
-            installButton.style.display = 'none';
-        });
-    }
+        </header>
+    `;
 }
+
+// Lógica para el botón de instalación
+document.addEventListener('DOMContentLoaded', () => {
+    let deferredPrompt;
+    const installButton = document.getElementById('install-button');
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+        deferredPrompt = e;
+        installButton.style.display = 'block';
+    });
+
+    installButton.addEventListener('click', async () => {
+        if (deferredPrompt) {
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            if (outcome === 'accepted') {
+                console.log('User accepted the install prompt');
+            }
+            deferredPrompt = null;
+        }
+        installButton.style.display = 'none';
+    });
+
+    // Verifica si la app ya está instalada
+    const isInstalled = window.matchMedia('(display-mode: standalone)').matches;
+
+    // Verifica si se accede desde Instagram
+    const isInstagram = /Instagram/.test(navigator.userAgent);
+
+    // Muestra el botón si no está instalada y se accede desde Instagram
+    if (!isInstalled || isInstagram) {
+        installButton.style.display = 'block';
+    }
+});
